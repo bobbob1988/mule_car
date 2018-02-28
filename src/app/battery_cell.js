04d80bad38354fd9be1821fa4de026fdf3436016
Oliver Su <oliver.su@sfmotors.com>
2018-02-27 21:09:42 -0500

    change battery cell's text positions

--- src/app/battery_cell.js
+++ src/app/battery_cell.js
@@ -16,14 +16,14 @@ class BatteryCell extends Component {
     var y = parseInt(this.props.y);
     var id = parseInt(this.props.id);
     var volt = parseInt(this.state.volt);
-    var text_y = y+34;
+    var text_y = y+84;
     var color = "yellowgreen";
     if (this.state.volt < 4.0){
       color = "tomato";
     } else if (this.state.volt < 9.0){
       color = "khaki";
     }
-    var id_x = x + 78;
+    var id_x = x + 100;
     if (id < 10) {
       id_x += 5;
     }
@@ -31,10 +31,10 @@ class BatteryCell extends Component {
     return (
         <g>
           <rect width="130" height="155" rx="5" ry="5" x={x} y={y} fill={color} stroke="lightgrey" strokeWidth="2" />
-          <text x={x+39} y={text_y} fill="darkslateblue" fontFamily="Agency FB" fontSize="20">
+          <text x={x+55} y={text_y} fill="darkslateblue" fontFamily="Agency FB" fontSize="28">
             {volt}v
           </text>
-          <text x={id_x} y={text_y+40} fill="darkslateblue" fontFamily="Arial" fontSize="12">
+          <text x={id_x} y={text_y+60} fill="darkslateblue" fontFamily="Arial" fontSize="12">
             [{id}]
           </text>
         </g>
