449d8de2a79fb1764b300699d26a733abf4c7076
Oliver Su <oliver.su@sfmotors.com>
2018-07-23 16:16:30 -0700

    add drive mode to settings page

--- mule-car-app/app/components/settings/index.js
+++ mule-car-app/app/components/settings/index.js
@@ -1,5 +1,21 @@
 import React, { Component } from 'react';
 import Switch from 'react-switch';
+import ButtonGroup from 'react-ions/lib/components/ButtonGroup/ButtonGroup'
+
+const drive_modes = [
+  {
+    value: "awd",
+    label: "AWD"
+  },
+  {
+    value: "rwd",
+    label: "RWD"
+  },
+  {
+    value: "fwd",
+    label: "FWD"
+  }
+]
 
 
 class Settings extends Component {
@@ -7,19 +23,41 @@ class Settings extends Component {
   constructor(){
     super();
     this.state = { 
-      battery_checked: true,
-      dual_motors_checked: false
+      BMS_debug_checked: true,
+      VCU_debug_checked: true,
+      MCUR0_debug_checked: true,
+      MCUF0_debug_checked: true,
+      default_drive_mode: 2
     };
-    this.handleBatteryDebug = this.handleBatteryDebug.bind(this);
-    this.handleDualMotors = this.handleDualMotors.bind(this);
+    this.handle_BMS_debug = this.handle_BMS_debug.bind(this);
+    this.handle_VCU_debug = this.handle_VCU_debug.bind(this);
+    this.handle_MCUF0_debug = this.handle_MCUF0_debug.bind(this);
+    this.handle_MCUR0_debug = this.handle_MCUR0_debug.bind(this);
+    this.handleDriveMode = this.handleDriveMode.bind(this);
   }
 
-  handleBatteryDebug(checked){
-    this.setState({ battery_checked: checked });
+  handle_BMS_debug(checked){
+    this.setState({ BMS_debug_checked: checked });
+  }
+
+  handle_VCU_debug(checked){
+    this.setState({ VCU_debug_checked: checked });
+  }
+
+  handle_MCUF0_debug(checked){
+    this.setState({ MCUF0_debug_checked: checked });
+  }
+
+  handle_MCUR0_debug(checked){
+    this.setState({ MCUR0_debug_checked: checked });
+  }
+
+  handle_BMS_debug(checked){
+    this.setState({ BMS_debug_checked: checked });
   }
   
-  handleDualMotors(checked){
-    this.setState({ dual_motors_checked: checked });
+  handleDriveMode(event, value){
+    console.log("drive mode selected: " + value);
   }
 
   render() {
@@ -31,26 +69,66 @@ class Settings extends Component {
 
         <div className="row top-20">
           <div className="col-md-auto">
-            <span>Battery Debug Mode</span>
+            <span>Drive Mode</span>
+          </div>
+          <div className="col">
+            <ButtonGroup
+              name="drive mode"
+              options={drive_modes}
+              defaultOption={this.state.default_drive_mode}
+              changeCallback={this.handleDriveMode}>
+            </ButtonGroup>
+          </div>
+        </div>
+
+        <div className="row top-20">
+          <div className="col-md-auto">
+            <span>BMS Debug Mode</span>
           </div>
           <div className="col">
           <Switch
-            onChange={this.handleBatteryDebug}
-            checked={this.state.battery_checked}
+            onChange={this.handle_BMS_debug}
+            checked={this.state.BMS_debug_checked}
             id="battery-debug"
           />
           </div>
         </div>
 
-        <div className="row top-10">
+        <div className="row top-20">
           <div className="col-md-auto">
-            <span>Dual Motors Mode</span>
+            <span>VCU Debug Mode</span>
           </div>
           <div className="col">
           <Switch
-            onChange={this.handleDualMotors}
-            checked={this.state.dual_motors_checked}
-            id="dual-motors"
+            onChange={this.handle_VCU_debug}
+            checked={this.state.VCU_debug_checked}
+            id="battery-debug"
+          />
+          </div>
+        </div>
+
+        <div className="row top-20">
+          <div className="col-md-auto">
+            <span>MCUF0 Debug Mode</span>
+          </div>
+          <div className="col">
+          <Switch
+            onChange={this.handle_MCUF0_debug}
+            checked={this.state.MCUF0_debug_checked}
+            id="battery-debug"
+          />
+          </div>
+        </div>
+
+        <div className="row top-20">
+          <div className="col-md-auto">
+            <span>MCUR0 Debug Mode</span>
+          </div>
+          <div className="col">
+          <Switch
+            onChange={this.handle_MCUR0_debug}
+            checked={this.state.MCUR0_debug_checked}
+            id="battery-debug"
           />
           </div>
         </div>
